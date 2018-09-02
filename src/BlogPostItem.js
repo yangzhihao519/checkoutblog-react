74cadf13efa57ae0cbf9594c1b6ea25e5099f80a
Zhihao Yang <yang_zhihao@live.com>
2018-09-02 21:56:22 +0100

    Format post date

--- src/BlogPostItem.js
+++ src/BlogPostItem.js
@@ -17,6 +17,9 @@ class BlogPostItem extends Component {
 
   render() {
     const { title, imageUrl, summary, postDate } = this.props.post
+    var dateToBeFormatted = new Date(postDate)
+    var dateFormatRule = { year: 'numeric', month: 'long', day: 'numeric' }
+    var formattedPostDate = dateToBeFormatted.toLocaleDateString('en-UK', dateFormatRule)
     return (
       <div className="blog-post-item">
           <div className="blog-post-item--image-container">
@@ -27,7 +30,7 @@ class BlogPostItem extends Component {
               <button className="blog-post-item--content--action-btn" type="button" onClick={this.edit}>Edit</button>
               <button className="blog-post-item--content--action-btn" type="button" onClick={this.remove}>Delete</button>
             </div>
-            <div className="blog-post-item--content--post-date">{postDate}</div>
+            <div className="blog-post-item--content--post-date">{formattedPostDate}</div>
             <div className="blog-post-item--content--title">{title}</div>
             <div className="blog-post-item--content--summary">{summary}</div>
           </div>
